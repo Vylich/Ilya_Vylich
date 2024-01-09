60546241abdca44b50833b58b55cdab20fec5f94
Vylich <ilya.sidorenko.99@gmail.com>
2024-01-09 20:40:50 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './assets/scss/style.scss'
+import Home from './assets/components/Home'
+
+function App() {
+
+  return (
+    <div className="page">
+      <div className="container">
+        <Home />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/Home.jsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom'
+function Home() {
+	return (
+		<>
+			<h1 className='home__title'>
+				<span>Hi, </span>
+				<br />
+				<span>My name is </span>
+				<br />
+				<span>Ilya</span>
+				<br />
+			</h1>
+			<nav className='home__nav'>
+				<ul className='nav'>
+					<li className='nav__item'>
+						<a data-link='1' href='#'>about</a>
+					</li>
+					<li className='nav__item'>
+						<a data-link='2' href='#'>portfolio</a>
+					</li>
+					<li className='nav__item'>
+						<a data-link='3' href='#'>experience</a>
+					</li>
+					<li className='nav__item'>
+						<a data-link='4' href='#'>links</a>
+					</li>
+				</ul>
+			</nav>
+			<div className='home__wrapper'>
+				<p data-text='1'>about</p>
+				<p data-text='2'>portfolio</p>
+				<p data-text='3'>experience</p>
+				<p data-text='4'>Links</p>
+			</div>
+		</>
+	)
+}
+
+export default Home
